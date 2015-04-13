3559cb7c7d26f6744a324a7ac5ddd90ed186ec89
u16suzu <u16suzu@gmail.com>
2015-04-13 14:14:08 +0900

    Fix bug : replace with new file at the monthly command.

--- monthly.rb
+++ monthly.rb
@@ -8,18 +8,14 @@ def file_name
 end
 
 def make_file_with_file_path( file_path )
-    # todo :
-     # ファイルが存在する場合は作らないようにしたほうがよいかもしれないけれども。
-     # 今のままでも上書きで新規作成されることはないので問題は無い。
-  p system "touch #{ file_path }"
+    unless( File.exist? (file_path))
+        system "touch #{ file_path }"
+    end
 end
 
 def open_file( path_and_file_name )
-  p system "/Applications/SublimeText2.app/Contents/SharedSupport/bin/subl -a #{ path_and_file_name }"
+    system "/Applications/SublimeText2.app/Contents/SharedSupport/bin/subl -a #{ path_and_file_name }"
 end
-
-p file_name
-p MemoPath::MEMO_DIR_MONTHLY
 
 path_and_file_name = MemoPath::MEMO_DIR_MONTHLY + file_name
 

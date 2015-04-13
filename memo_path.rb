915ab38c893e4372c344592a56570cc5eba24293
u16suzu <u16suzu@gmail.com>
2015-04-13 14:27:57 +0900

    Automaticaly change the dir by year.

--- memo_path.rb
+++ memo_path.rb
@@ -1,6 +1,8 @@
+require 'Date'
+
 module MemoPath
   EDITOR_PATH = "/Applications/SublimeText2.app/Contents/SharedSupport/bin/subl -a"
-  MEMO_DIR = "/Users/u16suzu/memoWork/00_daily/2015/"
+  MEMO_DIR = "/Users/u16suzu/memoWork/00_daily/#{ DateTime.now.strftime('%Y') }/"
   MEMO_DIR_DAILY        = MEMO_DIR + "daily/"
   MEMO_DIR_MONTHLY = MEMO_DIR + "monthly/"
   MEMO_DIR_ANNUAL    = MEMO_DIR + "annual/"

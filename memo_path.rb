29e16e696600cde4b751d6369678e9b0cb33c553
u16suzu <u16suzu@gmail.com>
2015-05-11 11:48:41 +0900

    Change the directory name to workMemo from memoWork.

--- memo_path.rb
+++ memo_path.rb
@@ -2,7 +2,7 @@ require 'Date'
 
 module MemoPath
   EDITOR_PATH = "/Applications/SublimeText2.app/Contents/SharedSupport/bin/subl -a"
-  MEMO_DIR = "/Users/u16suzu/memoWork/00_daily/#{ DateTime.now.strftime('%Y') }/"
+  MEMO_DIR = "/Users/u16suzu/workMemo/00_daily/#{ DateTime.now.strftime('%Y') }/"
   MEMO_DIR_DAILY        = MEMO_DIR + "daily/"
   MEMO_DIR_MONTHLY = MEMO_DIR + "monthly/"
   MEMO_DIR_ANNUAL    = MEMO_DIR + "annual/"

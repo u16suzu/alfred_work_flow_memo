52137be6b9d8535e563a304456d87ee830e5d2ac
u16suzu <u16suzu@gmail.com>
2015-06-05 18:52:20 +0900

    fix

--- memo_path.rb
+++ memo_path.rb
@@ -1,7 +1,7 @@
 require 'Date'
 
 module MemoPath
-  EDITOR_PATH = "/usr/local/bin/atom "
+  EDITOR_PATH = "/usr/local/bin/atom"
   MEMO_DIR = "/Users/u16suzu/workMemo/00_daily/#{ DateTime.now.strftime('%Y') }/"
   MEMO_DIR_DAILY        = MEMO_DIR + "daily/"
   MEMO_DIR_MONTHLY = MEMO_DIR + "monthly/"

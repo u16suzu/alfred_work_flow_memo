d4fe4213488e42ab8430036718441ecc478439f0
u16suzu <u16suzu@gmail.com>
2015-06-14 18:38:44 +0900

    wip

--- daily.rb
+++ daily.rb
@@ -1,5 +1,6 @@
 require 'Date'
 require './memo_path'
+require './memo_util'
 
 # 今日の日付のファイルを指定ディレクトリに作成する.
 
@@ -14,12 +15,8 @@ def make_file_with_file_path( file_path )
     end
 end
 
-def open_file( path_and_file_name )
-    system "#{ MemoPath::EDITOR_PATH } #{ path_and_file_name }"
-end
-
 path_and_file_name = MemoPath::MEMO_DIR_DAILY + file_name
 
 make_file_with_file_path( path_and_file_name )
 
-open_file( path_and_file_name )
+MemoUtil::open_file( path_and_file_name )
--- monthly.rb
+++ monthly.rb
@@ -1,5 +1,6 @@
 require 'Date'
 require './memo_path'
+require './memo_util'
 
 # 今月のファイルを指定ディレクトリに作成する.
 
@@ -14,13 +15,8 @@ def make_file_with_file_path( file_path )
     end
 end
 
-def open_file( path_and_file_name )
-    system "#{ MemoPath::EDITOR_PATH } #{ path_and_file_name }"
-end
-
 path_and_file_name = MemoPath::MEMO_DIR_MONTHLY + file_name
 
 make_file_with_file_path( path_and_file_name )
 
-open_file( path_and_file_name )
-
+MemoUtil::open_file( path_and_file_name )

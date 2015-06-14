1a1da377f5881d8093b47d0d79fbe868b40b6d50
u16suzu <u16suzu@gmail.com>
2015-06-14 18:52:37 +0900

    wip

--- memo_util.rb
+++ memo_util.rb
@@ -5,4 +5,10 @@ module MemoUtil
       system "#{ MemoPath::EDITOR_PATH } #{ path_and_file_name }"
   end
 
+  def self.make_file_with_file_path( file_path, template_file_path )
+      unless( File.exist? (file_path))
+          system "touch #{ file_path }"
+          system "cat #{ template_file_path } > #{ file_path }"
+      end
+  end
 end
--- monthly.rb
+++ monthly.rb
@@ -11,15 +11,9 @@ def template_file_path
   MemoPath::MEMO_DIR_TEMPLATE + '/template_monthly.md'
 end
 
-def make_file_with_file_path( file_path, template_file_path )
-    unless( File.exist? (file_path))
-        system "touch #{ file_path }"
-        system "cat #{ template_file_path } > #{ file_path }"
-    end
-end
 
 path_and_file_name = MemoPath::MEMO_DIR_MONTHLY + file_name
 
-make_file_with_file_path( path_and_file_name, template_file_path )
+MemoUtil::make_file_with_file_path( path_and_file_name, template_file_path )
 
 MemoUtil::open_file( path_and_file_name )
